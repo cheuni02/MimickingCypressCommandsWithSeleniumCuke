52f1a228fb94e85afaacd4190688ce9fae46b81d
cheuni02 <inhcheung@gmail.com>
2024-10-14 17:43:46 +0100

    feat(tests): first commit

---
+++ src/main/java/org/cheuni02/Main.java
@@ -0,0 +1,9 @@
+package org.cheuni02;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public String greet() {
+        return "Hello and welcome!";
+    }
+}
---
+++ src/test/resources/features/loadTest.feature
@@ -0,0 +1,8 @@
+Feature: As a Test
+  I am supposed to pass
+
+  Scenario: Test case 1
+    Given A precondition
+    When A change happens
+    Then Expected Outcome occurs
+    And Running Main.main returns 'Hello and welcome!'
