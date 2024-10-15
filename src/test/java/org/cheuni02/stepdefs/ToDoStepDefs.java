37cc5665d011bba5f4dbcf7e78c1ae507d1e2e8f
cheuni02 <inhcheung@gmail.com>
2024-10-15 01:35:34 +0100

    feat(tests): added 'Can add new todo items'

--- src/test/java/org/cheuni02/stepdefs/ToDoStepDefs.java
+++ src/test/java/org/cheuni02/stepdefs/ToDoStepDefs.java
@@ -3,15 +3,19 @@ package org.cheuni02.stepdefs;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,9 +26,14 @@ public class ToDoStepDefs {
     private final WebDriver driver;
 
     By todoList = By.cssSelector("ul.todo-list li");
+    By inputTodo = By.cssSelector("input.new-todo");
 
     public List<WebElement> elements(By locator) {
         return driver.findElements(locator);
+    }
+
+    public WebElement element(By locator) {
+        return driver.findElement(locator);
     }
 
     public ToDoStepDefs(Hooks hooks) {
@@ -44,10 +53,33 @@ public class ToDoStepDefs {
 
     @And("One of them is {string}")
     public void oneOfThemIsPayElectricBill(String task) throws InterruptedException {
-        List<String> taskStrings = new ArrayList<String>();
+        List<String> taskStrings = new ArrayList<>();
         for (WebElement ele : elements(todoList)) {
             taskStrings.add(ele.getText());
         }
         assertTrue(taskStrings.contains(task));
+    }
+
+    @When("I type a new Item {string}")
+    public void iTypeANewItemFeedTheCat(String task) throws InterruptedException {
+        new Actions(driver)
+                .sendKeys(element(inputTodo), task)
+                .sendKeys(Keys.ENTER)
+                .perform();
+    }
+
+    @And("Hit Enter key to add")
+    public void hitEnterKeyToAdd() throws InterruptedException {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Then("There are now {int} items")
+    public void thereAreNowItems(int numItems) throws InterruptedException {
+        assertEquals(elements(todoList).size(), numItems);
+    }
+
+    @And("Last item is {string}")
+    public void lastItemIsFeedTheCat(String task) {
+        assertEquals(elements(todoList).getLast().getText(), task);
     }
 }
--- src/test/resources/features/exampleToDoApp.feature
+++ src/test/resources/features/exampleToDoApp.feature
@@ -2,9 +2,19 @@ Feature: As a modern day clerk
   I need to be able to effectively list all to dos
   And be able to track them
 
+  Background:
+    Given I am on the todos page
+
   @Chrome
   Scenario: Displays two todo items by default
-    Given I am on the todos page
     Then There are 2 items by default
     And One of them is 'Pay electric bill'
     And One of them is 'Walk the dog'
+
+  @Chrome
+  Scenario: Can add new todo items
+    When I type a new Item 'Feed the cat'
+    And Hit Enter key to add
+    Then There are now 3 items
+    And Last item is 'Feed the cat'
+
